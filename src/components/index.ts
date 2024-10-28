0271930249d0064f0ca5ad698bce167f257e4d49
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-10-28 18:52:42 -0400

    +adds

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,5 +5,5 @@ export { default as ThemeProvider } from "./providers/Theme.tsx";
 /* Providers */
 
 /* Buttons */
-export { Button } from "./ui/button.tsx";
+export { Button } from "./ui/Button.tsx";
 /* Buttons */
