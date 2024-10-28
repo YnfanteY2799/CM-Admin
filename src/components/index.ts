70ab43d6f418da9266239abd59922c5ada94892b
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-10-28 18:49:09 -0400

    +adds

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,11 @@
+/* Providers */
+
 export { default as ThemeProvider } from "./providers/Theme.tsx";
+
+/* Providers */
+
+/* Buttons */
+
+export { default as Button } from "./ui/button.tsx";
+
+/* Buttons */
--- src/types/components.ts
+++ src/types/components.ts
@@ -1,7 +1,14 @@
-import type { ReactNode } from "react";
+import { ButtonVariants } from "@/utils/variants";
+
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+import type { VariantProps } from "class-variance-authority";
 
 /* Here May be located almost all of the main types of the app that belong to components  */
 
 export interface RSC {
   children: ReactNode;
+}
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof ButtonVariants> {
+  asChild?: boolean;
 }
