b450f5e76a66931d0487424b5590622f176afc50
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-10-28 17:24:29 -0400

    +adds

---
+++ src/types/components.ts
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+/* Here May be located almost all of the main types of the app that belong to components  */
+
+export interface IRSC {
+  children: ReactNode;
+}
