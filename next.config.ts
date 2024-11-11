3cb1d36c81d580f35fd8e3d659dc01c69e73af52
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-11 17:34:13 -0400

    +adds

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from "next";
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  optimizeDeps: {
-    exclude: ["@node-rs/argon2"],
-  },
-};
+/* config options here */
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
