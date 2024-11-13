d02e13ced67c467f2e78dc5c2ca984cb238a15c1
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-12 20:51:53 -0400

    +adds

--- src/api/Auth/index.ts
+++ src/api/Auth/index.ts
@@ -1,18 +1,30 @@
 "use server";
+import { getUserByEmail, globalPOSTRateLimit, RefillingTokenBucket, Throttler } from "@/utils/server";
 import { ObjectParser } from "@pilcrowjs/object-parser";
-import { globalPOSTRateLimit } from "@/utils/server";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 import type { PassKeyCredentialsObj } from "@/types/common";
 import type { TLoginFS } from "@/utils/common";
 
+const throttler = new Throttler<number>([1, 2, 4, 8, 16, 30, 60, 180, 300]);
+const ipBucket = new RefillingTokenBucket<string>(20, 1);
+
+// TODO: Assumes X-Forwarded-For is always included.
 export async function serviceBasedLogin({ email, password }: TLoginFS): Promise<Omit<any, "JWT">> {
   if (!globalPOSTRateLimit()) return { response: "not logged" };
+
+  const clientIP = (await headers()).get("X-Forwarded-For");
+  if (clientIP !== null && !ipBucket.check(clientIP, 1)) return { message: "Too many requests" };
+
+  const user = await getUserByEmail(email);
+  if (!user) return { message: "user not exists" };
+  if (!throttler.consume(user.id)) return { message: "Too many requests" };
+  
 
   return { response: "logged" };
 }
 
-export async function passKeyBasedLogin(data: PassKeyCredentialsObj): Promise<boolean> {
+export async function passKeyBasedLogin(data: PassKeyCredentialsObj): Promise<any> {
   if (!globalPOSTRateLimit()) return false;
 
   const parser = new ObjectParser(data);
--- src/utils/server/index.ts
+++ src/utils/server/index.ts
@@ -4,3 +4,8 @@ export * from "./RateLimit.ts";
 export * from "./WebAuthn.ts";
 export * from "./requests.ts";
 /* Bulk Exports */
+
+/* DB Methods */
+export * from "./db/User.ts";
+
+/* DB Methods */
