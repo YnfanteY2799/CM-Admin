65cbfd7e5a489d315a42172ec614d2dc327ecd9c
Yassett J Ynfante Jimenez <49245097+YnfanteY2799@users.noreply.github.com>
2024-11-15 19:59:43 -0400

    +adds

--- src/components/Switchers/LangSelectSwitcher.tsx
+++ src/components/Switchers/LangSelectSwitcher.tsx
@@ -1,28 +1,31 @@
 "use client";
 import { Dropdown, DropdownTrigger, Button, DropdownMenu, DropdownItem } from "@nextui-org/react";
-import { type ReactNode, useState, useTransition } from "react";
-import { Link, usePathname } from "@/i18n/routing";
+import { type ReactNode, useTransition } from "react";
+import { usePathname } from "@/i18n/routing";
 import FlagIcons from "./parts/FlagIcons";
 import { locales } from "@/i18n/routing";
 import { useLocale } from "next-intl";
 
 export default function LangSelectSwitcher(): ReactNode {
   const locale = useLocale();
-  const pathname = usePathname();
-  const otherLocale = locale === "en" ? "es" : "en";
 
   const [isPending, startTransition] = useTransition();
+
+  
+
 
   return (
     <Dropdown>
       <DropdownTrigger>
-        <Button variant="bordered" className="capitalize">
-          {pathname}
+        <Button variant="light" size="sm" className="capitalize">
+          {locale}
         </Button>
       </DropdownTrigger>
       <DropdownMenu aria-label="Dropdown Variants" color="primary" variant="light">
         {locales.map((x) => (
-          <DropdownItem key={x}>{x}</DropdownItem>
+          <DropdownItem key={x} startContent={<FlagIcons name={x} />}>
+            {x}
+          </DropdownItem>
         ))}
       </DropdownMenu>
     </Dropdown>
--- src/components/Switchers/parts/FlagIcons.tsx
+++ src/components/Switchers/parts/FlagIcons.tsx
@@ -1,11 +1,13 @@
-import { type ReactNode } from "react";
+import { NortAmericanFlag, SpainFlag } from "@/components";
+
+import type { ReactNode } from "react";
 
 export default function FlagIcons({ name }: { name: string }): ReactNode {
   switch (name) {
     case "en":
-      return <></>;
+      return <NortAmericanFlag />;
     case "es":
-      return <></>;
+      return <SpainFlag />;
     default:
       return <></>;
   }
